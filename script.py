7cf177e08c19c53056e3e4be0657c0ac2889fffa
Mucka <hugodelacour.pro@gmail.com>
2024-06-24 00:24:43 +0200

    fix : update data

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import os
 import re
 import unicodedata
+import json
 
 def normalize_filename(s):
     s = re.sub(r'[^\w\s-]', '', s).strip().lower()
@@ -10,11 +11,9 @@ def normalize_filename(s):
     s = unicodedata.normalize('NFKD', s).encode('ascii', 'ignore').decode('utf-8')
     return s
 
-def download_images_from_page(url):
+def download_images_from_page(url, image_data):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    
-    image_data = []
     
     img_tags = soup.find_all('a', class_='short-poster')
     for img in img_tags:
@@ -22,27 +21,25 @@ def download_images_from_page(url):
         alt_text = img.find('img')['alt']
         href = img['href']
         image_name = normalize_filename(alt_text)
-        image_data.append((image_url, image_name, href))
-    
-    with open('data.txt', 'a') as file:
-        for _, _, href in image_data:
+        image_data.append({
+            "url": image_url,
+            "name": image_name
+        })
+        
+        # Append the href to data.txt
+        with open('data.txt', 'a') as file:
             file.write(href + '\n')
-    
-    for image_url, image_name, _ in image_data:
-        try:
-            image_response = requests.get(image_url)
-            subdir = 'src'
-            if not os.path.exists(subdir):
-                os.makedirs(subdir)
-            image_path = os.path.join(subdir, f'{image_name}.jpg')
-            with open(image_path, 'wb') as image_file:
-                image_file.write(image_response.content)
-            print(f'Téléchargé {image_name}.jpg depuis {image_url}')
-        except Exception as e:
-            print(f'Erreur lors du téléchargement de {image_url}: {str(e)}')
 
 base_url = 'https://fr.fss.lol/films/page/'
+image_data = []
+
 for page_num in range(1, 1094):
     url = f'{base_url}{page_num}/'
     print(f'Téléchargement à partir de {url} ...')
-    download_images_from_page(url)
+    download_images_from_page(url, image_data)
+
+# Save image data to img.json
+with open('img.json', 'w', encoding='utf-8') as json_file:
+    json.dump(image_data, json_file, ensure_ascii=False, indent=4)
+
+print('Les données des images ont été sauvegardées dans img.json et les hrefs ont été ajoutés à data.txt')
